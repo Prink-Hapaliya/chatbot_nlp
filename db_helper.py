2b2f7cd160d498025dcdb6ac9bc947414d0fe15b
Prink Hapaliya <90205818+Prink-Hapaliya@users.noreply.github.com>
2023-11-10 18:55:57 +0530

    Add files via upload

---
+++ db_helper.py
@@ -0,0 +1,69 @@
+import mysql.connector
+
+cnx = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="prink@1604",
+    database="foodchatbotdb"
+)
+
+
+def insert_order_item(food_item, quantity, order_id):
+    try:
+        cursor = cnx.cursor()
+        cursor.callproc('insert_order_item', (food_item, quantity, order_id))
+        cnx.commit()
+        cursor.close()
+        print("Order item inserted successfully!")
+        return 1
+    except mysql.connector.Error as err:
+        print(f"Error inserting order item: {err}")
+        cnx.rollback()
+        return -1
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        cnx.rollback()
+        return -1
+
+
+def get_total_order_price(order_id):
+    cursor = cnx.cursor()
+    query = (f"SELECT get_total_order_price({order_id})")
+    cursor.execute(query)
+    result = cursor.fetchone()[0]
+    cursor.close()
+    return result
+
+
+def get_next_order_id():
+    cursor = cnx.cursor()
+    query = "SELECT Max(order_id) from orders"
+    cursor.execute(query)
+    result = cursor.fetchone()[0]
+    cursor.close()
+
+    if result is None:
+        return 1
+    else:
+        return result + 1
+
+
+def insert_order_tracking(order_id, status):
+    curses = cnx.cursor()
+    insert_query = "Insert into order_tracking (order_id,status) values (%s,%s)"
+    curses.execute(insert_query, (order_id, status))
+    cnx.commit()
+    curses.close()
+
+
+def get_order_status(order_id: int):
+    cursor = cnx.cursor()
+    query = "SELECT status FROM order_tracking WHERE order_id = %s"
+    cursor.execute(query, (order_id,))
+    result = cursor.fetchone()
+    cursor.close()
+
+    if result is not None:
+        return result[0]
+    else:
+        return None
